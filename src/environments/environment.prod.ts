fc19e81dae65003821e247fab71fc8a86ad3ed4d
Muhammad Awais <awaisshaikh94@gmail.com>
2022-02-20 15:49:24 +0500

    env published

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    // your firebase object
+    apiKey: "AIzaSyCF4KZe9hnb0JNC_CFAUKAerBclR9cI8m0",
+    authDomain: "car-wash-app-742f4.firebaseapp.com",
+    projectId: "car-wash-app-742f4",
+    storageBucket: "car-wash-app-742f4.appspot.com",
+    messagingSenderId: "232770482026",
+    appId: "1:232770482026:web:11486304c46fe84d9cff13",
+    measurementId: "G-48Q2SZL933"
   }
 };
